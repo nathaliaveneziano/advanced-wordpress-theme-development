2ede6e4e064df1e951a9e2573218c058d0efcf5f
Nathália Veneziano <nathaliaveneziano@icloud.com>
2022-09-24 16:45:03 -0300

    registering styles and scripts in functions file

--- functions.php
+++ functions.php
@@ -6,8 +6,22 @@
  * @package Advanced WordPress Theme Development
  */
 
-function pedagogia_concursos_enqueue_scripts()
+function advanced_theme_enqueue_scripts()
 {
-  wp_enqueue_style('stylesheet', get_stylesheet_uri() . '/style.css', [], filemtime(get_template_directory() . '/style.css'), 'all');
+  // Register Styles
+  wp_register_style('style', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
+  wp_register_style('bootstrap', get_template_directory_uri() . '/assets/src/css/bootstrap.min.css', [], false, 'all');
+
+  // Register Scripts
+  wp_register_script('main', get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true);
+  wp_register_script('bootstrap', get_template_directory_uri() . '/assets/src/js/bootstrap.bundle.min.js', ['jquery'], false, true);
+
+  // Enqueue Styles
+  wp_enqueue_style('style');
+  wp_enqueue_style('bootstrap');
+
+  // Enqueue Scripts
+  wp_enqueue_script('main');
+  wp_enqueue_script('bootstrap');
 }
-add_action('wp_enqueue_scripts', 'pedagogia_concursos_enqueue_scripts');
+add_action('wp_enqueue_scripts', 'advanced_theme_enqueue_scripts');
