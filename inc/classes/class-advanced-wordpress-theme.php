3a7fb592ee44cf570bacd85b8d4ea8371fd186cb
Nathália Veneziano <nathaliaveneziano@icloud.com>
2022-10-08 14:52:57 -0300

    add custom logo in navbar

--- inc/classes/class-advanced-wordpress-theme.php
+++ inc/classes/class-advanced-wordpress-theme.php
@@ -34,5 +34,12 @@ class ADVANCED_WORDPRESS_THEME
   public function setup_theme()
   {
     add_theme_support('title-tag');
+    add_theme_support('custom-logo', [
+      'header-text' => ['site-title', 'site-description'],
+      'height' => 50,
+      'width' => 100,
+      'flex-height' => true,
+      'flex-width' => true,
+    ]);
   }
 }
