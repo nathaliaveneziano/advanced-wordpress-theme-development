b6ced516c279d0ef96853680fd7baffc59079783
Nathália Veneziano <nathaliaveneziano@icloud.com>
2022-10-15 17:07:20 -0300

    customize posts

--- index.php
+++ index.php
@@ -9,8 +9,59 @@
 get_header();
 ?>
 
+<div id="primary">
+  <main id="main" class="site-main mt-5" role="main">
+    <?php
+    if (have_posts()) {
+    ?>
+      <div class="container">
+        <?php
+        if (is_home() && !is_front_page()) {
+        ?>
+          <header class="mb-5">
+            <h1 class="page-title">
+              <!-- screen-reader-text -->
+              <?php single_post_title(); ?>
+            </h1>
+          </header>
+        <?php
+        }
+        ?>
 
-<main>Main</main>
+        <div class="row">
+          <?php
+          $index = 0;
+          $number_columns = 3;
+
+          // Start the loop.
+          while (have_posts()) : the_post();
+            if ($index % $number_columns === 0) {
+          ?>
+              <div class="col-lg-4 col-md-6 col-sm-12">
+              <?php
+            }
+
+              ?>
+              <h3><?php the_title(); ?></h3>
+              <div><?php the_excerpt(); ?></div>
+              <?php
+
+              $index++;
+
+              if ($index !== 0 && $index % $number_columns === 0) {
+              ?>
+              </div>
+          <?php
+              }
+            endwhile;
+          ?>
+        </div>
+      </div>
+    <?php
+    }
+    ?>
+  </main>
+</div>
 
 <?php
 get_footer();
--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -16,7 +16,7 @@ $header_menus = wp_get_nav_menu_items($header_menu_id);
 ?>
 
 <nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
+  <div class="container">
     <?php
     if (function_exists('the_custom_logo')) {
       the_custom_logo();
@@ -25,63 +25,64 @@ $header_menus = wp_get_nav_menu_items($header_menu_id);
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <?php
-    if (!empty($header_menus) && is_array($header_menus)) {
-    ?>
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <?php
-        foreach ($header_menus as $menu_item) {
-          if (!$menu_item->menu_item_parent) {
-            $child_menu_items = $menu_class->get_child_menu_items($header_menus, $menu_item->ID);
-            $has_children = !empty($child_menu_items) && is_array($child_menu_items);
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <?php
+      if (!empty($header_menus) && is_array($header_menus)) {
+      ?>
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <?php
+          foreach ($header_menus as $menu_item) {
+            if (!$menu_item->menu_item_parent) {
+              $child_menu_items = $menu_class->get_child_menu_items($header_menus, $menu_item->ID);
+              $has_children = !empty($child_menu_items) && is_array($child_menu_items);
 
-            if (!$has_children) {
-        ?>
-              <li class="nav-item">
-                <a class="nav-link" href="<?= esc_url($menu_item->url); ?>">
-                  <?= esc_html($menu_item->title); ?>
-                </a>
-              </li>
-            <?php
-            } else {
-            ?>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  <?= esc_html($menu_item->title); ?>
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <?php
-                  foreach ($child_menu_items as $child_menu_item) {
-                  ?>
-                    <li><a class="dropdown-item" href="<?= esc_url($child_menu_item->url); ?>">
-                        <?= esc_html($child_menu_item->title); ?>
+              if (!$has_children) {
+          ?>
+                <li class="nav-item">
+                  <a class="nav-link" href="<?= esc_url($menu_item->url); ?>">
+                    <?= esc_html($menu_item->title); ?>
+                  </a>
+                </li>
+              <?php
+              } else {
+              ?>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <?= esc_html($menu_item->title); ?>
+                  </a>
+                  <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <?php
+                    foreach ($child_menu_items as $child_menu_item) {
+                    ?>
+                      <li><a class="dropdown-item" href="<?= esc_url($child_menu_item->url); ?>">
+                          <?= esc_html($child_menu_item->title); ?>
+                        </a></li>
+                    <?php
+                    }
+                    ?>
+                    <li>
+                      <hr class="dropdown-divider">
+                    </li>
+                    <li><a class="dropdown-item" href="<?= esc_url($menu_item->url); ?>">
+                        All <?= esc_html($menu_item->title); ?>
                       </a></li>
-                  <?php
-                  }
-                  ?>
-                  <li>
-                    <hr class="dropdown-divider">
-                  </li>
-                  <li><a class="dropdown-item" href="<?= esc_url($menu_item->url); ?>">
-                      All <?= esc_html($menu_item->title); ?>
-                    </a></li>
-                </ul>
-              </li>
-            <?php
+                  </ul>
+                </li>
+              <?php
+              }
+              ?>
+          <?php
             }
-            ?>
-        <?php
           }
-        }
-        ?>
-      </ul>
-    <?php
-    }
-    ?>
-    <form class="d-flex" role="search">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
-  </div>
+          ?>
+        </ul>
+      <?php
+      }
+      ?>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
   </div>
 </nav>
