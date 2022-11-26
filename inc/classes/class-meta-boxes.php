3e52061686ccb7e765949aa498c0bba5d2d4db10
Nathália Veneziano <nathaliaveneziano@icloud.com>
2022-11-26 19:52:02 -0300

    verify nonce in meta boxes

--- inc/classes/class-meta-boxes.php
+++ inc/classes/class-meta-boxes.php
@@ -48,6 +48,11 @@ class Meta_Boxes
   public function custom_meta_box_html($post)
   {
     $value = get_post_meta($post->ID, '_hide_page_title', true);
+
+    /**
+     * Use nonce for verification
+     */
+    wp_nonce_field(plugin_basename(__FILE__, 'hide_title_meta_box_nonce_name'))
 ?>
     <label for="advanced-theme-field">
       <?php esc_html_e('Hide the page title', 'advanced-theme'); ?>
@@ -68,6 +73,22 @@ class Meta_Boxes
 
   public function save_post_meta_data($post_id)
   {
+    /**
+     * When the post is saved or updated we get $_POST available
+     * CHeck if the current user is authorized
+     */
+
+    if (!current_user_can('edit_post', $post_id)) {
+      return;
+    }
+
+    /**
+     * Check if the nonce value we received is the same we created.
+     */
+    if (!isset($_POST['hide_title_meta_box_nonce_name']) || !wp_verify_nonce($_POST['hide_title_meta_box_nonce_name'], plugin_basename(__FILE__))) {
+      return;
+    }
+
     if (array_key_exists('advanced-theme_hide_title_field', $_POST)) {
       update_post_meta(
         $post_id,
