f47807fb7b58d75fa015d4ed20f0d4f67ec58cc5
Nathália Veneziano <nathaliaveneziano@icloud.com>
2022-11-28 17:42:44 -0300

    add the date posted

--- inc/helpers/template-tags.php
+++ inc/helpers/template-tags.php
@@ -36,3 +36,29 @@ function the_post_custom_thumbnail($post_id, $size = 'featured-large', $addition
 {
   echo get_the_post_custom_thumbnail($post_id, $size, $additional_attributes);
 }
+
+function advanced_theme_post_on()
+{
+  $time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+
+  // Post is modified (when post published time is not equal to post modified time)
+  if (get_the_time('U') !== get_the_modified_time('U')) {
+    $time_string =
+      '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+  }
+
+  $time_string = sprintf(
+    $time_string,
+    esc_attr(get_the_date(DATE_W3C)),
+    esc_attr(get_the_date()),
+    esc_attr(get_the_modified_date(DATE_W3C)),
+    esc_attr(get_the_modified_date())
+  );
+
+  $posted_on = sprintf(
+    esc_html_x('Posted on %1$s', 'post date', 'advanced-theme'),
+    '<a href="' . esc_url(get_permalink()) . '" rel="bookmark">' . $time_string . '</a>'
+  );
+
+  echo '<span class="posted-on text-secondary">' . $posted_on . '</span>';
+}
---
+++ template-parts/components/blog/entry-meta.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Template for entry meta.
+ *
+ * @package Advanced WordPress Theme Development
+ */
+?>
+
+<div class="entry-meta mb-3">
+  <?php advanced_theme_post_on(); ?>
+</div>
