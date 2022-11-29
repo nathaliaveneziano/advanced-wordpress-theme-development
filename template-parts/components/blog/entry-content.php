c7c86fc7c8f23a952bde96595e7a46cb7f3cc22b
Nathália Veneziano <nathaliaveneziano@icloud.com>
2022-11-29 17:18:55 -0300

    add the excerpt with size

---
+++ template-parts/components/blog/entry-content.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Template for entry content.
+ * To be used inside WordPress The Loop.
+ *
+ * @package Advanced WordPress Theme Development
+ */
+?>
+
+<div class="entry-content">
+  <?php
+  if (is_single()) {
+    the_content(
+      sprintf(
+        wp_kses(
+          __('Continue reading %s <span class="meta-nav">&rArr;</span>', 'advanced-theme'),
+          [
+            'span' => ['class' => []]
+          ]
+        ),
+        the_title('<span class="screen-reader-text">', '</span>', false)
+      )
+    );
+  } else {
+    advanced_theme_the_excerpt(150);
+  }
+  ?>
+</div>
